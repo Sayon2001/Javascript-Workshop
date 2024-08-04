45107ab9a71224070ec25cdb8f3f1587c925530b
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-08-04 19:14:45 +0545

    learn in-depth about arrays and objects

--- script.js
+++ script.js
@@ -29,6 +29,33 @@ friends.splice(2,0,"Suhana")
 
 console.log(friends)
 
+
+//slice --> used to create a copy of portion of array. doesn't modify original array
+const numbers = [1,2,3,4,5,6]
+const newNumbers = numbers.slice(0,5)
+console.log(numbers)
+console.log(newNumbers)
+
+//splice
+numbers.splice(2,1,100,101)
+console.log(numbers)
+
+//split --> split string into array
+const text = "I love JavaScript"
+console.log(text)
+newArray = text.split(" ")
+console.log(newArray)
+
+
+//task using splice
+const languages = ["php", "js", "c"]
+languages.splice(1,1,"html")
+console.log(languages)
+
+const characters = ['m','a','n','i','s','h']
+characters.splice(0,6,13,1,14,9,19,8)
+console.log(characters)
+
 //object
 
 const name = "Sayon Shrestha"
@@ -43,3 +70,25 @@ const person = {
     address : "Dharan",
     isNepali : true
 }
+console.log(person.name)
+console.log(person.isNepali)
+
+person.college = "Koshi St. James College"
+
+console.log(person)
+
+Object.freeze(person)   //to freeze the object so that it key value can't be changed
+person.name = "Changed Name"
+console.log(person)     //will still print old name because object is frozen
+
+console.log(Object.keys(person))    //will print the keys of the object(person)
+console.log(Object.values(person))  //will print the values of the object(person)
+
+//casing
+/*
+ihateprogramminginfortan
+camelCase --> iHateProgrammingInFortan
+snake_case --> i_hate_programming_in_fortan //mostly used in python
+kebab-case --> i-hate-programming-in-fortan 
+PascalCase --> IHateProgrammingInFortan //mostly used in oop languages like Java
+*/
