e5c2e7de916038382d60bf1fde58a530e0697160
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-08-05 19:11:04 +0545

    learn about loops, and conditional statements

--- script.js
+++ script.js
@@ -86,9 +86,66 @@ console.log(Object.values(person))  //will print the values of the object(person
 
 //casing
 /*
-ihateprogramminginfortan
+UPPERCASE --> IHATEPROGRAMMINGINFORTAN
+lowercase --> ihateprogramminginfortan
 camelCase --> iHateProgrammingInFortan
 snake_case --> i_hate_programming_in_fortan //mostly used in python
 kebab-case --> i-hate-programming-in-fortan 
 PascalCase --> IHateProgrammingInFortan //mostly used in oop languages like Java
 */
+
+//conditional statements
+
+const isRaining = true
+
+if(isRaining){
+    console.log("Carry an umbrella")
+} else {
+    console.log("Don't carry an umbrella")
+}
+
+//ternary operator
+
+const isSunny = true
+// isSunny ? console.log("It's a sunny day") : console.log("It's a rainy day")
+console.log(isSunny ? "It's a sunny day" : "It's a rainy day")
+
+//short circuit
+isSunny && console.log("It's a sunny day") //if isSunny is true then this is executed
+isSunny || console.log("It's a rainy day") //if isSunny is false then this is executed
+
+const temperature = 10
+if(temperature > 30){
+    console.log("It's hot outside")
+}
+else if(temperature > 25){
+    console.log("It's warm outside")
+}
+else{
+    console.log("It's cold outside")
+}
+
+
+//nullish coelescing operator
+
+let age1 = null
+let age2 = 25
+let age3 = undefined
+
+console.log(age1 ?? "Age is not provided")
+console.log(age2 ?? "Age is not provided")
+console.log(age3 ?? "Age is not provided")
+
+//loops
+
+const vowels = ['a','e','i','o','u']
+
+//for loop
+for(let i=0; i<vowels.length; i++){
+    console.log(vowels[i])
+}
+
+//for of
+for(let vowel of vowels){
+    console.log(vowel)
+}
