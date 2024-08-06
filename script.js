3f0bbe29e1e48c72c6a2ef770135335bb13a64a4
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-08-06 21:51:33 +0545

    learn about while, do while, and for in loops

--- script.js
+++ script.js
@@ -149,3 +149,42 @@ for(let i=0; i<vowels.length; i++){
 for(let vowel of vowels){
     console.log(vowel)
 }
+
+//while loop
+
+let i = 0
+while(i < vowels.length){
+    console.log(vowels[i])
+    i++
+}
+
+i=0
+while (i < 10) {
+    console.log("The number is " + i)
+    i++;
+}
+
+//do while loop
+i=0
+do{
+    console.log(vowels[i])
+    i++;
+}while (i<vowels.length) 
+
+i=0
+do {
+    console.log("The number is " + i)
+    i++;
+} while (i < 10);
+
+//for in
+const members = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30
+}
+
+for(let key in members){
+    console.log(key + " is " + members[key])
+}
+
